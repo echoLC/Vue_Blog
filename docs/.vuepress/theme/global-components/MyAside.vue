55ae291d24d29013badc6ace7b8943df1981338f
echoLC <651644100@qq.com>
2019-05-16 21:01:23 +0800

    Add tag menu

--- docs/.vuepress/theme/global-components/MyAside.vue
+++ docs/.vuepress/theme/global-components/MyAside.vue
@@ -39,6 +39,13 @@
           slot="title"
         >{{$themeConfig.menus.home || '主页'}}</span>
       </el-menu-item>
+      <el-menu-item index="/tags/">
+        <i class="iconfont icon-tags"></i>
+        <span
+          class="item-title"
+          slot="title"
+        >{{$themeConfig.menus.tags || '标签分类'}}</span>
+      </el-menu-item>
       <el-menu-item index="/archieve/">
         <i class="iconfont icon-archive"></i>
         <span
