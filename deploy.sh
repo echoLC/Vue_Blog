8bfcbb52a51cadeade900f5d82cb6decadeb1e4b
echoLC <651644100@qq.com>
2019-05-16 17:57:50 +0800

    Modify the deploy sh

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,11 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd ./dist
+cd ./docs/dist
 
-
-git init
 git add -A
-git commit -m 'deploy'
+git commit -m "$1"
 
 
 # 把下面的push命令按照你的情况修改后去掉注释
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/echoLC/echolc.github.com.git master
